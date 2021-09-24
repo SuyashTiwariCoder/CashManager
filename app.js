9aa86b28ca3879f6d60ff54eb4fa0368f08b2aa7
SuyashTiwariCoder <89460164+SuyashTiwariCoder@users.noreply.github.com>
2021-09-24 12:02:52 +0530

    bugs cleared

--- app.js
+++ app.js
@@ -3,29 +3,48 @@ let cashGiven = document.querySelector("#cashGiven");
 let checkBtn = document.querySelector("#checkBtn");
 let message = document.querySelector("#message");
 let notesToShow = document.querySelectorAll(".numberOfNotes");
-const availableNotes = [2000, 1000, 500, 100, 50, 20, 10, 5, 1];
+let table = document.querySelector(".table");
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+
+
+
+
+
 checkBtn.addEventListener("click", function clickEventHandler() {
   message.style.display = "none";
-  if (billAmount.value > 0) {
-    if (cashGiven.value > billAmount.value) {
-      const amountToReturn = cashGiven.value - billAmount.value;
+if (Number(billAmount.value) > 0 && Number(cashGiven.value)>0) {
+  
+  
+    if (Number(cashGiven.value) > Number(billAmount.value)) {
+      const amountToReturn = Number(cashGiven.value) - Number(billAmount.value);
       calculateChange(amountToReturn);
     } else {
       showMessage("The Bill amount can not be greater than cash amount  ");
     }
-  } else {
-    showMessage("Invalid Bill Amount ");
-  }
+  } 
+
+
+else {
+  alert("Invalid Amont , Values must be positive or greater than zero")
+}
+
 });
 
+
+
+
 function calculateChange(amountToReturn) {
+  
   for (let i = 0; i < availableNotes.length; i++) {
     const numberOfNotes = Math.trunc(amountToReturn / availableNotes[i]);
 
-    amountToReturn = amountToReturn % availableNotes[i];
+    amountToReturn = amountToReturn% availableNotes[i];
     notesToShow[i].innerText = numberOfNotes;
   }
 }
+
+
 
 function showMessage(msg) {
   message.style.display = "block";
